7b01f5416edf89ebef89ace4247af6e6a69f9951
Hatsuyuki <minecraftLSY@126.com>
2023-04-20 07:00:16 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 $seed = time();
 $num = rand(1,172);
-$picpath = "https://cdn.jsdelivr.net/hatsuyuki2333/phimg/api/img/".$num.".png";
+$picpath = "https://cdn.jsdelivr.net/hatsuyuki2333/phi-api@resource/img/".$num.".png";
 die(header("Location: $picpath"));
 ?>
